57868623599dcbbb48f1db92dedcb095296ee23d
Icaro <icarostn@gmail.com>
2020-08-31 13:46:27 -0300

    fix inverse kinematics equations

--- 2-dof/admittance/Adm_TwoDOF_Cartesian.m
+++ 2-dof/admittance/Adm_TwoDOF_Cartesian.m
@@ -1,4 +1,4 @@
-%% Control - 2 DoF Robotic Arm
+% Control - 2 DoF Robotic Arm
 %{
 Departamento de Engenharia Mecânica
 Escola de Engenharia de São Carlos - Universidade de São Paulo
@@ -8,174 +8,205 @@ clc;
 clear all;
 close all;
 addpath('..\..\shared-functions')
+isTaskSpace = 1;
+isImpedance = 0;
 
-%% Play animation?
+% Play animation?
 play = 1;
-SEA = 0;
+isBackdrivable = 0;
 
 % Model parameters
 [t, dt] = SimulationTime(0,6,5e-3);
 
-%% Variables
-run('Adm_TwoDOF_Variables.m')
+% Variables
+run('TwoDOF_Variables.m')
+q_k = zeros(2,length(t));
 
-%% Desired trajectory
-offset = [-pi/2; -pi/8];
-A = 1;
+% Desired trajectory
+offset = [-pi/3; -pi/8];
+A = 60*pi/180;
 w = 0.5*pi;
 
 % First joint
 q_d(1,:) = A*sin(w*t) + offset(1);
-dq_d(1,:) = w*A*cos(w*t);
+dq_d(1,:) = 0*w*A*cos(w*t);
 % Second joint
 q_d(2,:) = 0*A*sin(w*t) + offset(2);
 dq_d(2,:) = 0*w*A*cos(w*t);
 
 % Desired trajectory
-for i = 1 : length(t)
-    if t(i) >= 1, q_d(1,i) = 1 + offset(1); end
-    
-    if t(i) >= 4, q_d(1,i)  = A*cos(w*t(i)) + offset(1); end
-    
-    if t(i) >= 5, q_d(1,i) = 0 + offset(1); end
-end
+% for i = 1 : length(t)
+%     if t(i) >= 1, q_d(1,i) = 1 + offset(1); end
+%
+%     if t(i) >= 4, q_d(1,i)  = A*cos(w*t(i)) + offset(1); end
+%
+%     if t(i) >= 5, q_d(1,i) = 0 + offset(1); end
+% end
 
 x_d = l(1)*cos(q_d(1,:)) + l(2)*cos(q_d(1,:) + q_d(2,:));
 y_d = l(1)*sin(q_d(1,:)) + l(2)*sin(q_d(1,:) + q_d(2,:));
+X_d = [x_d; y_d];
 
 % Initial conditions
-q_m(1,1) = offset(1);
+q(1,1) = offset(1);
 dq_m(1,1) = A*w;
 
-q_m(2,1) = offset(2);
+q(2,1) = offset(2);
 dq_m(2,1) = 0*A*w;
 
-%% Force sensor/Environment dynamics
-x_w = 0.5;	%m
+% Force sensor/Environment dynamics
+x_w = [0.6; 10];	%m
 dx_w = 0;   %m/s
-K_fs = 100;	%N/m        %(Ott, 2010)
-B_fs = 10;  %N.s/m      
+K_env = diag([100; 0]);	%N/m        %(Ott, 2010)
+B_env = diag([5; 0]);  %N.s/m
 
 %Wall is modeled as spring-damper system in Clover, 1999
 % In Seraji 1994, B_env = 10, K_env = 100. K_env varia depois para 25.
 
-%% Admittance controller parameters (stiff wall)
-K_d = 15;	%N/m
-B_d = 5;	%N.s/m
+% Admittance controller parameters (stiff wall)
+K_d = 5;	%N/m
+B_d = 3;	%N.s/m
 M_d = 0.01;	%N.s^2/m
 
-%% PID Gains (300, 15, 0, sem SEA)
-k_p = diag([1500 100]);	%N/m
-k_d = diag([30 10]);	%N.s/m
-k_i = diag([10 0]);      %N/m.s
+% PID Gains (300, 15, 0, without SEA)
+k_p = diag([200 150]);	%N/m
+k_d = diag([20 15]);	%N.s/m
+k_i = diag([0 0]);      %N/m.s
 
-%% Simulation (Forward Dynamics)
+g = 0;
+
+% Simulation (Forward Dynamics)
 for i = 1 : length(t) - 1
     % Matrices
-    [H, G, C, F] = TwoDOF_InvDyn(q_m(:,i), dq_m(:,i), l, m, g);
+    [H, G, C, F] = TwoDOF_InvDyn(q(:,i), dq(:,i), l, m, g);
     
     % Gravity compensator
     T_g(:, i) = G;
     
     % Jacobian (T = J'*F)
-    Jacobian = JacobianMatrix(l,q_m(:,i),dof);
+    Jacobian = JacobianMatrix(l,q(:,i),dof);
+    
     
     % End-effector coordinates
-    [coord_x, coord_y] = L_forward(l, q_m(:,i), dof);
-    x(:,i) = [coord_x; coord_y];
-    x_m(:,i) = coord_x;
-    y_m(:,i) = coord_y;
+    [coord_x, coord_y] = L_forward(l, q(:,i), dof);
+    X(:,i) = [coord_x; coord_y];
+    x(:,i) = coord_x;
+    y(:,i) = coord_y;
+    
     % End-effector velocities
-    dx(:,i) = Jacobian*dq_m(:,i);
+    dX(:,i) = Jacobian*dq(:,i);
     
-    %% Wall detection
-    if(OnCollisionEnter(x(:,i), x_w))
-        F_int = OnCollisionStay(x(:,i), dx(:,i), x_w, dx_w, K_fs, B_fs, 1, SEA, q_m(:,i), dq_m(:,i), q_d(:,i), l);
-        F_int = [F_int(2); F_int(1)];
-        F_ext(:,i) = -F_int;
+    if x(1,i) > x_w(1)
+        F_ext(:,i) = (K_env*(X(:,i) - x_w) + B_env*dX(:,i));
+        
+        if F_ext(1,i) < 0 || F_ext(2,i) < 0
+            F_ext(:,i) = [0;0];
+        end
+        
+        if i > 1
+            F_ext(:,i) = K_env*(2*(x(:,i) - x_w) - 1.5*(x(:,i-1) - x_w));
+            F_ext_filtered(:,i) = 2*F_ext(:,i) - 1.5*F_ext(:,i - 1);
+            
+            if F_ext_filtered(1,i) < 0 || F_ext_filtered(2,i) < 0
+                F_ext_filtered(:,i) = [0;0];
+            end
+        end
+        if i > 2
+           F_ext(:,i) = K_env*(2*(x(:,i) - x_w) - 1.5*(x(:,i-1) - x_w) + 0.5*(x(:,i-2) - x_w));
+            F_ext_filtered(:,i) = 2*F_ext(:,i) - 1.5*F_ext(:,i - 1) + 0.5*F_ext(:,i - 2);
+            
+            if F_ext_filtered(1,i) < 0 || F_ext_filtered(2,i) < 0
+                F_ext_filtered(:,i) = [0;0];
+                
+            end
+            
+        end
     end
-
-    F_error(:,i) = F_d(:,i) - F_ext(:,i);
+    F_error(:,i) = F_d(:,i) - F_ext_filtered(:,i);
     
-    %% Admittance Control Law
+    %F_error(:,i) = F_d(:,i) - F_ext(:,i);
+    
+    % Admittance Control Law
     if i == 1 && K_d ~= 0
-        x_v(:,i) = F_error(:,i)./K_d;
-    end
+        x_v(:,i) = F_error(:,i)./K_d; end
     
     if i > 1 && B_d ~= 0
-        x_v(:,i) = (F_error(:,i) + x_v(:,i - 1).*B_d/dt)./(B_d/dt + K_d);
-    end
+        x_v(:,i) = (F_error(:,i) + x_v(:,i - 1).*B_d/dt)./(B_d/dt + K_d); end
     
     if i > 2
-        x_v(:,i) = (F_error(:,i) + x_v(:,i - 1).*(2*M_d/(dt^2) + B_d/dt) - x_v(:,i - 2).*M_d/(dt^2))./(M_d/(dt^2) + B_d/dt + K_d);
+        x_v(:,i) = (F_error(:,i) + x_v(:,i - 1).*(2*M_d/(dt^2) + B_d/dt) - x_v(:,i - 2).*M_d/(dt^2))./(M_d/(dt^2) + B_d/dt + K_d); end
+    
+
+    % Inverse Kinematics
+    
+    if i ~= 1
+        q_k(:,i) = inv(Jacobian)*x_v(:,i) + 0*q_k(:,i-1);
     end
     
-    %q_v(2,i) = atan2(x_v(2,i), x_w);
+    %{
+    x0 = l(1)*cos(q(1,i));
+    y0 = l(1)*sin(q(1,i));
     
-    %q_v(2,i) = acos(x_v(2,i)/l(2)) - q_m(1,i);
+    if x_v(1,i) < -(x(:,i) + l(2))
+        x_v(1,i) = -(x(:,i) + l(2));
+    end
+    
+    x_prime = x(:,i) + x_v(1,i);
     
     if(F_error(1,i) ~= 0 || F_error(2,i) ~= 0)
-        q_v(2,i) = atan2(x_v(2,i),x_v(1,i)) + q_m(1,i);
-        q_v_pure(i) = atan2(x_v(2,i),x_v(1,i));
+        %{
+        if l(2)^2 < (x_prime - x0)^2
+            x_v(2,i) = y0;
+        else
+            % y = sqrt( l^2 - (x - x0) ^2 ) + y0;
+            x_v(2,i) = sqrt( l(2)^2 - ( x_prime - x0  )^2 ) + y0;
+        end
+        q_v(2,i) = atan2(x_v(2,i),x_v(1,i));
+        %}
+        delta_x = x_v(1,i) - x(:,i);
+        
+        if delta_x <= l(2)*cos(q(1,i)) + l(2)
+            
+            delta_x = l(2)*cos(q(1,i)) + l(2);
+            
+        end
+        q_v(2,i) = -acos(delta_x - l(2)*cos(q(1,i)) / l(2) );
     end
     
     if i > 1
         dq_v(2,i) = ( q_v(2,i) - q_v(2,i-1) )/ dt;
     end
+    %}
     
+    q_v(:,i) = [0 ; q_k(1,i) + q_k(2,i)];
     
-    %% PID Position control
+    % PID Position control
     q_r(:,i) = q_d(:,i) + q_v(:,i);
-    q_error(:, i) = q_r(:,i) - q_m(:,i) ;
-    q_error_pure(:,i) = q_d(:,i) - q_m(:,i);
-    dq_error(:, i) = dq_d(:,i) - dq_m(:,i) + 0*dq_v(:,i);
-
+    q_error(:, i) = q_r(:,i) - q(:,i) ;
+    dq_error(:, i) = dq_d(:,i) - dq(:,i);
+    
     if i ~= 1
-        int_error(:, i) = int_error(:, i - 1) + (q_error(:,i) + q_error(:, i - 1))*dt/2;
-    end
+        int_error(:, i) = int_error(:, i - 1) + (q_error(:,i) + q_error(:, i - 1))*dt/2; end
     
     T_a(:, i) = k_p * q_error(:, i) + k_d * dq_error(:, i) + k_i * int_error(:, i) + T_g(:, i);
+    %T_a(:, i) = Saturation( T_a(:, i), torque_max, torque_min);
+    %T_a(1,i) = 0;
     
-    %% Simulation
-    ddq_m(:, i) = H\(T_d(:, i) + T_a(:,i) - C*dq_m(:, i) - G - F*dq_m(:, i)  - Jacobian'*F_ext(:,i));
-    dq_m(:, i + 1) = dq_m(:, i) + ddq_m(:, i)*dt;
-    q_m(:, i + 1) = q_m(:, i) + dq_m(:, i)*dt + 0.5*ddq_m(:, i)*dt*dt;
+    % Simulation
+    ddq(:, i) = H\(T_d(:, i) + T_a(:,i) - C*dq(:, i) - G - F*dq(:, i)  - isBackdrivable*Jacobian'*F_ext(:,i));
+    dq(:, i + 1) = dq(:, i) + ddq(:, i)*dt;
+    q(:, i + 1) = q(:, i) + dq(:, i)*dt + 0.5*ddq(:, i)*dt*dt;
     
 end
 
-x_m(end) = x_m(end-1);
-y_m(end) = y_m(end-1);
-
-%% Animation
-q = q_m;
-L = l;
+% Animation
 if(play)
-    run('Animation_Adm_TwoDOF.m')
+    run('Animation_2DoF.m')
 end
 
-%% Plots
-run('Adm_TwoDOF_Plots.m')
+% Plots
+run('TwoDOF_Plots.m')
 
 return
-%% Analysis
+% Analysis
 run('Analysis.m')
-
-%% Filtering data
-windowSize = 2; 
-b = (1/windowSize)*ones(1,windowSize);
-a = 1;
-x = q_v;
-LPB = LowPassButterworth
-%%
-y = filter(LPB,x);
-
-figure
-plot(t,x)
-hold on
-plot(t,y)
-legend('Input Data','Filtered Data')
-%%
-figure
-Fs = 1/dt;
-Hpsd = dspdata.psd(x(1,:),'Fs',Fs)
-plot(Hpsd)
--- 2-dof/impedance/Imp_TwoDOF_Cartesian.m
+++ 2-dof/impedance/Imp_TwoDOF_Cartesian.m
@@ -1,4 +1,4 @@
-%% Control - 2 DoF Robotic Arm
+% Control - 2 DoF Robotic Arm
 %{
 Departamento de Engenharia Mecânica
 Escola de Engenharia de São Carlos - Universidade de São Paulo
@@ -8,107 +8,121 @@ clc;
 clear all;
 close all;
 addpath('..\..\shared-functions')
+isImpedance = 1;
+isTaskSpace = 1;
 
-%% Play animation?
+% Play animation?
 play = 1;
-SEA = 0;
+isBackdrivable = 0;
 
 % Model parameters
 [t, dt] = SimulationTime(0,6,5e-3);
+% Variables
+run('TwoDOF_Variables.m')
 
-%% Variables
-run('Adm_TwoDOF_Variables.m')
-dF_error = zeros(2,length(t));
-F_r = zeros(2,length(t));
-
-%% Desired trajectory
-offset = [-pi/2; -pi/8];
-A = 1;
+% Desired trajectory
+offset = [-pi/3; -pi/8];
+A = 60*pi/180;
 w = 0.5*pi;
 
 % First joint
 q_d(1,:) = A*sin(w*t) + offset(1);
-dq_d(1,:) = w*A*cos(w*t);
+dq_d(1,:) = 0*w*A*cos(w*t);
 % Second joint
-%q_d(2,:) = 0*A*sin(w*t) + offset(2);
-%dq_d(2,:) = 0*w*A*cos(w*t);
+q_d(2,:) = 0*A*sin(w*t) + offset(2);
+dq_d(2,:) = 0*w*A*cos(w*t);
 
 % Desired trajectory
-for i = 1 : length(t)
-    if t(i) >= 1, q_d(1,i) = 1 + offset(1); end
-    
-    if t(i) >= 4, q_d(1,i)  = A*cos(w*t(i)) + offset(1); end
-    
-    if t(i) >= 5, q_d(1,i) = 0 + offset(1); end
-end
+% for i = 1 : length(t)
+%     if t(i) >= 1, q_d(1,i) = 1 + offset(1); end
+%     
+%     if t(i) >= 4, q_d(1,i)  = A*cos(w*t(i)) + offset(1); end
+%     
+%     if t(i) >= 5, q_d(1,i) = 0 + offset(1); end
+% end
 
 x_d = l(1)*cos(q_d(1,:)) + l(2)*cos(q_d(1,:) + q_d(2,:));
 y_d = l(1)*sin(q_d(1,:)) + l(2)*sin(q_d(1,:) + q_d(2,:));
+X_d = [x_d; y_d];
 
 % Initial conditions
-q_m(1,1) = offset(1);
-dq_m(1,1) = A*w;
+q(1,1) = offset(1);
+dq(1,1) = A*w;
 
-q_m(2,1) = offset(2);
-dq_m(2,1) = 0*A*w;
+q(2,1) = offset(2);
+dq(2,1) = 0*A*w;
 
-%% Force sensor/Environment dynamics
-x_w = 0.5;	%m
+% Environment dynamics
+x_w = 0.6;	%m
 dx_w = 0;   %m/s
-K_fs = 1000;  %N/m
-B_fs = 10;   %N.s/m
+K_env = 1e4;  %N/m
+B_env = 10;   %N.s/m
 
-%% Impedance controller parameters (stiff wall)
-K_d = 100;	%N/m
-B_d = 5;	%N.s/m
-M_d = 0;	%N.s^2/m
+% Impedance controller parameters (stiff wall)
+K_d = 100*diag(2);	%N/m
+B_d = 10*diag(2);	%N.s/m
+M_d = 0*diag(2);	%N.s^2/m
 
-%% PID Gains (300, 15, 0, sem SEA)
-k_p = diag([100 150]);	%N/m
-k_d = diag([1 1]);      %N.s/m
+% PID Gains (300, 15, 0, without SEA)
+k_p = diag([20 20]);	%N/m
+k_d = diag([0 0]);      %N.s/m
 k_i = diag([0 0]);      %N/m.s
 
-%% Simulation (Forward Dynamics)
+g = 0;
+% Simulation (Forward Dynamics)
 for i = 1 : length(t) - 1
     % Matrices
-    [H, G, C, F] = TwoDOF_InvDyn(q_m(:,i), dq_m(:,i), l, m, g);
+    [H, G, C, F] = TwoDOF_InvDyn(q(:,i), dq(:,i), l, m, g);
     
     % Gravity compensator
     T_g(:, i) = G;
     
     % Jacobian (T = J'*F)
-    Jacobian = JacobianMatrix(l,q_m(:,i),dof);
+    Jacobian = JacobianMatrix(l,q(:,i),dof);
     
     % End-effector coordinates
-    [coord_x, coord_y] = L_forward(l, q_m(:,i), dof);
-    x(:,i) = [coord_x; coord_y];
-    x_m(:,i) = coord_x;
-    y_m(:,i) = coord_y;
+    [coord_x, coord_y] = L_forward(l, q(:,i), dof);
+    X(:,i) = [coord_x; coord_y];
+    x(:,i) = coord_x;
+    y(:,i) = coord_y;
     
     % End-effector velocities
-    dx(:,i) = Jacobian*dq_m(:,i);
+    dX(:,i) = Jacobian*dq(:,i);
     
-    F_r_x(i) = K_d*(x_d(i) - x_m(i)) - B_d*(dx(1,i));
-    F_r_y(i) = K_d*(y_d(i) - y_m(i)) - B_d*(dx(2,i));
+    F_r (:,i) = K_d * (X_d(:,i) - X(:,i) ) - B_d*dX(:,i);
     
-    F_r(:,i) = [F_r_x(i); F_r_y(i)];
-    
-    %% Wall detection
-    if(OnCollisionEnter(x(:,i), x_w))
-        F_int = OnCollisionStay(x(:,i), dx(:,i), x_w, dx_w, K_fs, B_fs, 1, SEA, q_m(:,i), dq_m(:,i), q_d(:,i), l);
-        F_int = [F_int(2); F_int(1)];
-        F_ext(:,i) = F_int;
-    end
-    
-    if(x_m(i) > x_w)
-        F_ext(:,i) = [K_fs*(x_m(i) - x_w) + B_fs*dx(2,i); 0];
+    % Wall detection
+    if(x(i) > x_w)
+        F_ext(:,i) = [K_env*(x(i) - x_w) + B_env*dX(2,i); 0];
+        F_ext(2,i) = 0;
         if F_ext(1,i) < 0
             F_ext(1,i) = 0;
         end
+
+        if i > 1
+            %F_ext(:,i) = K_env*(2*(x_m(i) - x_w) - 1.5*(x_m(i-1) - x_w));
+            F_ext_filtered(:,i) = 2*F_ext(:,i) - 1.5*F_ext(:,i - 1);
+            
+            if F_ext_filtered(1,i) < 0 || F_ext_filtered(2,i) < 0
+                %F_ext_filtered(:,i) = [0;0];
+            end
+        end
+        if i > 2
+            %F_ext(:,i) = K_env*(2*(x_m(i) - x_w) - 1.5*(x_m(i-1) - x_w) + 0.5*(x_m(i-2) - x_w));
+            F_ext_filtered(:,i) = 2*F_ext(:,i) - 1.5*F_ext(:,i - 1) + 0.5*F_ext(:,i - 2);
+            
+            if F_ext_filtered(1,i) < 0 || F_ext_filtered(2,i) < 0
+                %F_ext_filtered(:,i) = [0;0];
+                
+            end
+            
+        end
     end
     
-    %% PID Force control
+    
+    % PID Force control
     F_error(:,i) = F_r(:,i) + F_d(:,i) - F_ext(:,i);
+    F_error(:,i) = F_r(:,i) + F_d(:,i) - F_ext_filtered(:,i);
     
     if i ~= 1
         %int_error(:, i) = int_error(:, i - 1) + (F_error(:,i) + F_error(:, i - 1))*dt/2;
@@ -118,62 +132,34 @@ for i = 1 : length(t) - 1
     %T_a(:, i) = k_p * q_error(:, i) + k_d * dq_error(:, i) + k_i * int_error(:, i) + T_g(:, i);
     
     T_a(:,i) = Jacobian' * k_p * F_error(:,i)  + Jacobian' * k_d * dF_error(:,i) + T_g(:, i);
-    
+    T_a(:, i) = Saturation( T_a(:, i), torque_max, torque_min);
     %T_a(:,i) = Jacobian'*F_r(:,i);
-    %% Simulation
-    ddq_m(:, i) = H\(T_d(:, i) + T_a(:,i) - C*dq_m(:, i) - G - F*dq_m(:, i)  - 0*Jacobian'*F_ext(:,i));
-    dq_m(:, i + 1) = dq_m(:, i) + ddq_m(:, i)*dt;
-    q_m(:, i + 1) = q_m(:, i) + dq_m(:, i)*dt + 0.5*ddq_m(:, i)*dt*dt;
+    % Simulation
+    ddq(:, i) = H\(T_d(:, i) + T_a(:,i) - C*dq(:, i) - G - F*dq(:, i)  - isBackdrivable*Jacobian'*F_ext_filtered(:,i));
+    dq(:, i + 1) = dq(:, i) + ddq(:, i)*dt;
+    q(:, i + 1) = q(:, i) + dq(:, i)*dt + 0.5*ddq(:, i)*dt*dt;
     
 end
 
-x_m(end) = x_m(end-1);
-y_m(end) = y_m(end-1);
-
-%% Animation
-L = l;
-q = q_m;
+% Animation
 if(play)
-    run('Animation_Adm_TwoDOF.m')
+    run('Animation_2DoF.m')
 end
-%%
+%
+run('TwoDOF_Plots.m')
+return
 figure
 plot(t,F_r')
 title('Impedance forces')
 figure
+plot(t,T_a')
+title('Actuator forces')
+figure
 plot(t,F_ext')
 title('Contact forces')
 figure
-plot(t,x_d,t,x_m)
+plot(t,x_d,t,x)
 title('X trajectory (desired and real)')
 figure
-plot(t,y_d,t,y_m)
+plot(t,y_d,t,y)
 title('Y trajectory (desired and real)')
-%% Plots
-%{
-run('Adm_TwoDOF_Plots.m')
-
-%% Analysis
-run('Analysis.m')
-
-%% Filtering data
-windowSize = 2; 
-b = (1/windowSize)*ones(1,windowSize);
-a = 1;
-x = q_v;
-LPB = LowPassButterworth
-%%
-
-y = filter(LPB,x);
-
-figure
-plot(t,x)
-hold on
-plot(t,y)
-legend('Input Data','Filtered Data')
-%%
-figure
-Fs = 1/dt;
-Hpsd = dspdata.psd(x(1,:),'Fs',Fs)
-plot(Hpsd)
-%}
